1a14110cd8a6705dab7e70dd4590d6595444a038
Sabelo200027 <141147424+Sabelo200027@users.noreply.github.com>
2023-10-23 19:53:25 +0000

    First Half

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -7,10 +7,15 @@ import {
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import {UserList} from "./users";
+import { PostList } from "./posts";
+
 
 export const App = () => ( 
 <Admin dataProvider={dataProvider}>
   <Resource name= "Users" list={ListGuesser}/>
-  <Resource name= "users" list={UserList}/>
+  
+  <Resource name= "Students" list={UserList}/>
+  <Resource name="posts" list={ListGuesser} />
+  <Resource name="posts" list={PostList} />
 </Admin>
 );
